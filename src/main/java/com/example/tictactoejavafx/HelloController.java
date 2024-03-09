252a3de87bbe0cd03f3f156dcd6c16315a7aafd2
miklrz <arz.mikhail@gmail.com>
2024-03-09 13:08:29 +0300

    checkWin func

--- src/main/java/com/example/tictactoejavafx/HelloController.java
+++ src/main/java/com/example/tictactoejavafx/HelloController.java
@@ -5,27 +5,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+import java.util.Objects;
+
 public class HelloController {
 
     String nextSymbol = "x";
     String[][] arr = new String[3][3];
-
     boolean checkWin(String[][] arr){
-        boolean flag = false;
-        for (int y = 0; y < 3; ++y){
-            for(int x = 1; x < 3; ++x){
-                if(arr[x][y] != arr[x-1][y]) break;
-                if(x == 2) return true;
-            }
+        if((arr[0][0] == arr[0][1] && arr[0][0] == arr[0][2]&&arr[0][0] !=null)||(arr[1][0] == arr[1][1] && arr[1][0] == arr[1][2]&&arr[1][1] !=null)||(arr[2][0] == arr[2][1] && arr[2][0] == arr[2][2]&&arr[2][2] !=null)){
+            return true;
         }
-        for (int x = 0; x < 3; ++x){
-            for(int y = 1; y < 3; ++y){
-                if(arr[x][y] != arr[x][y-1]) break;
-                if(y == 2) return true;
-            }
+        if((arr[0][0] == arr[1][0] && arr[0][0] == arr[2][0]&&arr[0][0] !=null)||(arr[0][1] == arr[1][1] && arr[0][1] == arr[2][1]&&arr[1][1] !=null)||(arr[0][2] == arr[1][2] && arr[0][2] == arr[2][2]&&arr[2][2] !=null)){
+            return true;
+        }
+        if((arr[0][0] == arr[1][1] && arr[0][0] == arr[2][2] && arr[0][0] !=null) || (arr[0][2] == arr[1][1] && arr[0][2] == arr[2][0] &&arr[0][2]!=null)){
+            return true;
         }
         return false;
-
     }
     @FXML
     void onHelloButtonClick(ActionEvent event) {
@@ -34,7 +30,7 @@ public class HelloController {
         int x = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
         int y = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
         arr[x][y] = nextSymbol;
-
+        if(checkWin(arr)) System.out.println("win");
         if(nextSymbol == "x") nextSymbol = "o";
         else nextSymbol = "x";
     }
