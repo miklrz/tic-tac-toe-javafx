dfdff6ebb3dedefffcb9e7be4d721ee337320dba
miklrz <arz.mikhail@gmail.com>
2024-03-09 12:32:52 +0300

    doing checkWin func

--- src/main/java/com/example/tictactoejavafx/HelloController.java
+++ src/main/java/com/example/tictactoejavafx/HelloController.java
@@ -1,14 +1,45 @@
 package com.example.tictactoejavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
+    String nextSymbol = "x";
+    String[][] arr = new String[3][3];
+
+    boolean checkWin(String[][] arr){
+        boolean flag = false;
+        for (int y = 0; y < 3; ++y){
+            for(int x = 1; x < 3; ++x){
+                if(arr[x][y] != arr[x-1][y]) break;
+                if(x == 2) return true;
+            }
+        }
+        for (int x = 0; x < 3; ++x){
+            for(int y = 1; y < 3; ++y){
+                if(arr[x][y] != arr[x][y-1]) break;
+                if(y == 2) return true;
+            }
+        }
+        return false;
+
+    }
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void onHelloButtonClick(ActionEvent event) {
+        Button btn = (Button) event.getSource();
+        btn.setText(nextSymbol);
+        int x = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
+        int y = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
+        arr[x][y] = nextSymbol;
+
+        if(nextSymbol == "x") nextSymbol = "o";
+        else nextSymbol = "x";
+    }
+    @FXML
+    void initialize(){
+
     }
 }
